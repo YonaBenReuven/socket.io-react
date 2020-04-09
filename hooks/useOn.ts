384b1fe3b23613b9ecdbb8acb79cd3755d9f64c9
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-04-09 22:07:31 +0300

    useOn fix again

--- hooks/useOn.ts
+++ hooks/useOn.ts
@@ -1,7 +1,7 @@
 import { useEffect, useCallback } from "react";
 import { useSocket } from ".";
 
-const useOn = (event: string, fn: (...args: any[]) => any, deps: React.DependencyList | undefined): (...args: any[]) => any => {
+const useOn = (event: string, fn: (...args: any[]) => any, deps?: React.DependencyList): (...args: any[]) => any => {
 
     const socket = useSocket();
 
@@ -13,7 +13,7 @@ const useOn = (event: string, fn: (...args: any[]) => any, deps: React.Dependenc
         };
     }, []);
 
-    const memoizedFn = useCallback<(...args: any[]) => any>(fn, deps)
+    const memoizedFn = useCallback<(...args: any[]) => any>(fn, deps || []);
 
     return deps ? memoizedFn : fn;
 };
